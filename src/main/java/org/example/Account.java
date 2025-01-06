2c5e82d83571eba008fb2b6e7f805550f50a46a5
Edon Sokoli <edosok18@htl-shkoder.com>
2025-01-06 12:26:07 +0100

    Banking/

--- src/main/java/org/example/Account.java
+++ src/main/java/org/example/Account.java
@@ -1,2 +1,32 @@
-package org.example;public class Account {
+package org.example;
+
+public class Account {
+    private int balance;
+
+    public void deposit(int amount) {
+        if (amount > 0) {
+            balance += amount;
+        }
+    }
+
+    public void withdraw(int amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+        }
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void displayBalance() {
+        System.out.println("Balance: " + balance);
+    }
+
+    public void transfer(Account recipient, int amount) {
+        if (amount > 0 && amount <= balance) {
+            withdraw(amount);
+            recipient.deposit(amount);
+        }
+    }
 }
--- src/main/java/org/example/AccountTest.java
+++ src/main/java/org/example/AccountTest.java
@@ -1,2 +1,38 @@
-package org.example;public class AccountTest {
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+class AccountTest {
+
+    @Test
+    void deposit() {
+        Account account = new Account();
+        account.deposit(100);
+        account.displayBalance();
+    }
+
+    @Test
+    void withdraw() {
+        Account account = new Account();
+        account.deposit(2044);
+        account.withdraw(20);
+        account.displayBalance();
+    }
+
+    @Test
+    void displayBalance() {
+        Account account = new Account();
+        account.deposit(1044);
+        account.displayBalance();
+    }
+
+    @Test
+    void transfer() {
+        Account account1 = new Account();
+        Account account2 = new Account();
+        account1.deposit(44);
+        account1.transfer(account2, 82);
+        account1.displayBalance();
+        account2.displayBalance();
+    }
 }
